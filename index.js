3c5b65a71faaeecf11fc119bbe0b0473abf717b1
szhn <sergeybolbat05@gmail.com>
2021-11-02 15:34:09 +0300

    1

---
+++ index.js
@@ -0,0 +1,49 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const { SlashCreator, GatewayServer } = require('slash-create');
+const { Client } = require('discord.js');
+const { Player } = require('discord-player');
+const { registerPlayerEvents } = require('./events');
+const { generateDocs } = require('./docs');
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        'GUILDS',
+        'GUILD_VOICE_STATES'
+    ]
+});
+
+client.player = new Player(client);
+registerPlayerEvents(client.player);
+
+const creator = new SlashCreator({
+    applicationID: process.env.DISCORD_CLIENT_ID,
+    token: process.env.DISCORD_CLIENT_TOKEN,
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log('Generating docs...');
+    generateDocs(creator.commands);
+});
+
+creator
+    .withServer(
+        new GatewayServer(
+            (handler) => client.ws.on('INTERACTION_CREATE', handler)
+        )
+    )
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+if (process.env.DISCORD_GUILD_ID) creator.syncCommandsIn(process.env.DISCORD_GUILD_ID);
+else creator.syncCommands();
+
+client.login(process.env.DISCORD_CLIENT_TOKEN);
+
+module.exports = {
+    client,
+    creator
+};
